61fcb8347b75daa60bcd69ff99395ae92e51d4a2
fengluoye2012 <1522988552@qq.com>
2021-04-07 23:31:21 +0800

    协程

--- app/src/main/java/com/fly/mvvm/MainActivity.kt
+++ app/src/main/java/com/fly/mvvm/MainActivity.kt
@@ -3,6 +3,7 @@ package com.fly.mvvm
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.fly.mvvm.ui.main.MainFragment
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,8 +12,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
         }
+
+        GlobalScope.launch {
+
+        }
+
+        GlobalScope.launch(Dispatchers.Main){
+           val image = suspendingGetImage("")
+            //todo
+        }
+
+    }
+
+    //
+    suspend fun suspendingGetImage(id:String) = withContext(Dispatchers.IO){
+        delay(500)
+        return@withContext
     }
 }
